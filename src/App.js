29a4aa8ff59119beaef285d2c32bf77031e13fb5
SAGAR VARMA GOTTUMUKKALA <sagarvarma9867@gmail.com>
2023-04-09 12:08:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{useState} from "react";
+function App() {
+  useState();
+  useState();
+  
+  return (
+    <div className="App">
+      <h1>hello hooks</h1>
+    </div>
+  );
+}
+
+export default App;
